f7b1875111ddc863f6f4e15d33878ea5dda0cc5d
Leegeonwoo7 <workgeon07@naver.com>
2023-12-22 21:35:53 +0900

    연결리스트: 노드 삭제

--- data_structure/MyLinkedList.java
+++ data_structure/MyLinkedList.java
@@ -12,5 +12,37 @@ public class MyLinkedList {
 		MyListNode newNode = new MyListNode(data);
 		
 		newNode.link = preNode.link;
+		preNode.link = newNode;
+	}
+	
+	//노드 삽입(마지막에 삽입)
+	public void insertNode(String data) {
+		MyListNode newNode = new MyListNode(data);
+		if(head == null) {
+			this.head = newNode;
+		}else {
+			MyListNode tempNode = head;
+			while(tempNode.link != null) {
+				tempNode = tempNode.link;
+			}
+			tempNode.link = newNode;
+		}
+	}
+	
+	//노드 삭제(중간 노드)
+	public void deletNode(String data) {
+		MyListNode preNode = head;
+		MyListNode tempNode = head.link;
+		
+		if(data.equals(preNode.getData())) {
+			head = preNode.link;
+			preNode.link = null;
+		}else {
+			while(tempNode != null) {
+				if(data.equals(tempNode.getData())) {
+					
+				}
+			}
+		}
 	}
 }
