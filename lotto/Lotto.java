9c25915dbb6264b602699829261a02d87568f1bf
Leegeonwoo7 <workgeon07@naver.com>
2023-12-26 20:33:38 +0900

    로또 생성 및 중복제거

---
+++ lotto/Lotto.java
@@ -0,0 +1,23 @@
+package lotto;
+
+public class Lotto {
+
+	public static void main(String[] args) {
+		int[] lotto = new int[6];
+		
+		for(int i=0; i<lotto.length; i++) {
+			int random = (int)(Math.random() * 45 + 1);
+			lotto[i] = random;
+			for(int j=0; j<i; j++) {
+				if(lotto[i] == lotto[j]) {
+					i--;
+					break;
+				}
+			}
+		}
+		
+		
+
+	}
+
+}
