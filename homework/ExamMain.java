eed59beb9bcab3ddfde2455971155d7347d168f1
Leegeonwoo7 <workgeon07@naver.com>
2023-12-30 15:50:57 +0900

    Exam 클래스의 main클래스

---
+++ homework/ExamMain.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class ExamMain {
+
+	public static void main(String[] args) {
+		Exam exam = new Exam();
+		
+		System.out.println("이름\t1 2 3 4 5\t점수");
+		exam.printInfo();
+	}
+
+}
